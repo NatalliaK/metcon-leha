1cffcc0be3937811f8fbecfcc9a85ec81936a8ba
NatalliaK <studina-natali@yandex.by>
2017-04-29 21:55:29 +0300

    Изменения и дополнения в package.json и gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var plumber = require("gulp-plumber");
+var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
 
@@ -8,10 +9,17 @@ var server = require("browser-sync").create();
 gulp.task('sass', function() {
 	gulp.src('sass/style.scss')
 		.pipe(plumber())
-		.pipe(sass(),
+		.pipe(sass())
+		.pipe(postcss([
 			autoprefixer({browsers: [
-				"last 2 versions"
-			]}))
+				"last 1 version",
+				"last 2 Chrome versions",
+				"last 2 Firefox versions",
+				"last 2 Opera versions",
+				"last 2 Edge versions"
+			]})
+		]))
+
 		.pipe(gulp.dest('css'))
 		.pipe(server.stream());
 });
@@ -26,8 +34,10 @@ gulp.task("serve", ["sass"], function() {
 	});
 
 	gulp.watch("sass/**/*.scss", ["sass"]);
-	gulp.watch("*.html").on("change", server.reload);
-	gulp.watch("*.js").on("change", server.reload);
+	gulp.watch("*.html")
+		.on("change", server.reload);
+	gulp.watch("*.js")
+		.on("change", server.reload);
 });
 
 gulp.task("default", ["sass", "serve"]);
